81b53b8d4c84b9c22fda86336243421706de53e6
Alex <romanovaser@mail.ru>
2023-02-04 21:07:40 +0300

    add localstorage

--- test 3/client/src/redux/store.js
+++ test 3/client/src/redux/store.js
@@ -1,16 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import eventsReducer from './slices/eventsSlice';
 import activeEditReducer from './slices/modalEditSlice';
 import oneEventReducer from './slices/oneEventSlice';
 import reminderEventReducer from './slices/reminderEventSlice';
 
-const store = configureStore({
-  reducer: {
-    events: eventsReducer,
-    activeEdit: activeEditReducer,
-    oneEvent: oneEventReducer,
-    reminder: reminderEventReducer,
-  },
+const rootReducer = combineReducers({
+  events: eventsReducer,
+  activeEdit: activeEditReducer,
+  oneEvent: oneEventReducer,
+  reminder: reminderEventReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
+
+export const persistor = persistStore(store);
 
 export default store;
